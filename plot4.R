f9ca6ede9c79e9c4155125f809e008e1c492eda4
Mike Taylor <mikecolintaylor@gmail.com>
2015-02-05 16:53:44 +0000

    finished plot 4

---
+++ plot4.R
@@ -0,0 +1,32 @@
+
+setwd("G:/Coursera/exploratoryDataAnalysis/Assignment1/ExData_Plotting1")
+
+#download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "household_power_consumption.zip")
+
+options(stringsAsFactors = F)
+hhPower <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE)
+
+hhPower$Date <- as.Date(hhPower$Date, format = "%d/%m/%Y")
+
+hhPower <- subset(hhPower, as.Date("2007-02-01") <= Date & Date <= as.Date("2007-02-02"))
+hhPower$Time <- format(strptime(hhPower$Time, "%H:%M:%S"), "%H:%M:%S")
+
+hhPower$DateTime <- as.POSIXlt(paste(hhPower$Date, hhPower$Time))
+
+str(hhPower)
+
+png("plot4.png")
+par(bg = NA, mfrow = c(2, 2))
+with(hhPower, plot(hhPower$DateTime, Global_active_power, type = "l",
+                   xlab = "", ylab = "Global active power (kilowatts)"))
+with(hhPower, plot(hhPower$DateTime, Voltage, type = "l",
+                   xlab = "datetime"))
+with(hhPower, plot(hhPower$DateTime, Sub_metering_1, type = "l", col = "black",
+                   xlab = "", ylab = "Energy sub metering"))
+with(hhPower, lines(hhPower$DateTime, Sub_metering_2, type = "l", col = "red"))
+with(hhPower, lines(hhPower$DateTime, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", col = c("black", "red", "blue"), lwd = c(1, 1, 1),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+with(hhPower, plot(hhPower$DateTime, Global_reactive_power, type = "l",
+                   xlab = "datetime"))
+dev.off()
